4683ef9f573b86cd73abb5584c31e53c30c7d6e3
000Player-Dev <jose.dev000@gmail.com>
2021-03-29 18:40:00 -0600

    First and Last Commit

---
+++ src/components/search/SearchScreen.js
@@ -0,0 +1,94 @@
+import React, { useMemo } from 'react';
+import queryString from 'query-string';
+import { useLocation } from 'react-router';
+import { useForm } from '../../hooks/useForm';
+import { HeroCard } from '../heroes/HeroCard';
+import { getHeroesByName } from '../../selectors/getHeroesByName';
+
+
+export const SearchScreen = ({ history }) => {
+
+    
+    const location = useLocation();
+    const { q = '' } = queryString.parse( location.search );
+    
+    const [ formValues, handleInputChange ] = useForm( {
+        searchText: q
+    });
+    
+    const { searchText } = formValues;
+    const heroesFiltered = useMemo(() => getHeroesByName( q ), [q]);
+
+
+    const handleSearch = (e) => {
+        e.preventDefault();
+        history.push(`?q=${ searchText }`);
+    }
+
+    return (
+        <div>
+            <h1>SearchScreen</h1>
+            <hr />
+
+            <div className="row">
+                <div className="col-4">
+                    <h4>Search Form</h4>
+                    <hr />
+
+                    <form onSubmit={ handleSearch }>
+                        <input 
+                            type="text"
+                            placeholder="Find your hero"
+                            className="form-control"
+                            autoComplete="off"
+                            name="searchText"
+                            value={ searchText }
+                            onChange={ handleInputChange }
+                        />
+
+                        <button 
+                            type="submit"
+                            className="btn m-1 btn-block btn-outline-primary"
+                        >
+                            Search...
+                        </button>
+
+                    </form>
+                </div>
+
+                <div className="col-7 animate__animated animate__fadeIn">
+
+                    <h4> Results </h4>
+                    <hr />
+
+                    {
+                        ( q === '')
+                            && 
+                            <div className="alert alert-info">
+                                Search a hero
+                            </div>
+                    }
+
+                    {
+                        ( q !== '' && heroesFiltered.length === 0)
+                            &&
+                            <div className="alert alert-danger">
+                                There's no a hero named: { q }
+                            </div>
+                    }
+
+                    {
+                        heroesFiltered.map( hero => (
+                            <HeroCard 
+                                key={hero.id}
+                                {...hero}
+                            />
+                        ))
+                    }
+
+                </div>
+            </div>
+
+        </div>
+    )
+}
